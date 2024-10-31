dd2886654e9836c753ff3270a51b95e3c9bf62e5
ljxpython <1030470148@qq.com>
2024-11-01 00:14:59 +0800

    增加locust的init使用demo

--- locustfiles/test_demo/test_good.py
+++ locustfiles/test_demo/test_good.py
@@ -10,11 +10,22 @@ import random
 import sys
 import time
 
-from locust import HttpUser, between, task
+from locust import HttpUser, between, task,events
 from loguru import logger
 
 from src.utils.locust_report import manual_report, measure
 
+
+@events.init.add_listener
+def on_locust_init(environment, **kwargs):
+    # 获取并发大小
+    num = environment.parsed_options.num_users
+    logger.info(f"并发数: {num}")
+    # 如果并发数大于10,将其修改为10
+    if num > 10:
+        environment.parsed_options.num_users = 10
+    # 打印修改后的压测数
+    logger.info(f"修改后的压测数{environment.parsed_options.num_users}")
 
 class QuickstartUser(HttpUser):
     wait_time = between(1, 5)
@@ -30,3 +41,7 @@ class QuickstartUser(HttpUser):
         with manual_report("world"):
             logger.info(f"请求路径: /api/auto_pytest/get_suite_list,")
             time.sleep(random.randint(1, 3))
+            logger.info(self.environment.parsed_options)
+            # 打印当前的测试并发量
+            current_users = self.environment.runner.user_count
+            logger.info(f"当前并发量: {current_users}")
